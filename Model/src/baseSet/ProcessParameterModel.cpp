d99ef63015bc3baca066f4c43e3605cd6e131e34
wangzhen <kevinkingmin@outlook.com>
2025-11-29 14:19:22 +0800

    样本针充盈放到加样本界面配置

--- Model/src/baseSet/ProcessParameterModel.cpp
+++ Model/src/baseSet/ProcessParameterModel.cpp
@@ -12,7 +12,6 @@ const QString ProcessParameterModel::SAMPLING_CODE = "L1904";
 const QString ProcessParameterModel::DRAINING_CODE = "L1905";
 const QString ProcessParameterModel::PAUSING_CODE = "L1906";
 const QString ProcessParameterModel::TAKE_PHOTO_CODE = "L1907";
-const QString ProcessParameterModel::SAMPLE_NEEDLE_FILLING_CODE = "L1908";
 
 ProcessParameterModel::ProcessParameterModel()
     :_id(0)
@@ -26,7 +25,7 @@ ProcessParameterModel::ProcessParameterModel()
     ,_bedShakingStrt(0,0)
     ,_drainingStrt(0)
     ,_pausingStrt("")
-    ,_samplingStrt(0)
+    ,_samplingStrt()
     ,_dryingStrt(1000, 2, 1000, 37, 1000)
 {
 }
@@ -164,13 +163,19 @@ bool ProcessParameterModel::strToSampling(ProcessParameterModel::SamplingStrt& o
     QJsonDocument document = QJsonDocument::fromJson(str.toUtf8(), &parse_error);
     if (document.isNull() ||(parse_error.error != QJsonParseError::NoError)) return false;
     auto obj = document.object();
-    out._sampleUl=obj.value("sampleUl").toDouble();
+    out._sampleUl = obj.value("sampleUl").toDouble(10);
+    out._isFilling = obj.value("isFilling").toBool(false);
+    out._innerTime = obj.value("innerTime").toInt(3);
+    out._outerTime = obj.value("outerTime").toInt(3);
     return true;
 }
 QString ProcessParameterModel::SamplingToStr(const SamplingStrt &strt)
 {
     QJsonObject obj;
     obj.insert("sampleUl",strt._sampleUl);
+    obj.insert("isFilling", strt._isFilling);
+    obj.insert("innerTime", strt._innerTime);
+    obj.insert("outerTime", strt._outerTime);
 
     QJsonDocument doc;
     doc.setObject(obj);
@@ -217,18 +222,6 @@ bool ProcessParameterModel::strToDrying(ProcessParameterModel::DryingStrt& out, 
     return true;
 }
 
-bool ProcessParameterModel::strToSampleNeedleFilling(SampleNeedleFillingStrt &out, const QString &str)
-{
-    if(str.isNull() || str.isEmpty()) return false;
-    QJsonParseError parse_error;
-    QJsonDocument document = QJsonDocument::fromJson(str.toUtf8(), &parse_error);
-    if (document.isNull() ||(parse_error.error != QJsonParseError::NoError)) return false;
-    auto obj = document.object();
-    out._innerTime=obj.value("innerTime").toInt();
-    out._outerTime=obj.value("outerTime").toInt();
-    return true;
-}
-
 QString ProcessParameterModel::dryingToStr(const DryingStrt &strt)
 {
     QJsonObject obj;
@@ -237,18 +230,6 @@ QString ProcessParameterModel::dryingToStr(const DryingStrt &strt)
     obj.insert("fanLevel",strt._fanLevel);
     obj.insert("heatTime",strt._heatTime);
     obj.insert("bedTemperature",strt._bedTemperature);
-
-    QJsonDocument doc;
-    doc.setObject(obj);
-    QString jsonStr=doc.toJson(QJsonDocument::Compact);
-    return jsonStr;
-}
-
-QString ProcessParameterModel::sampleNeedleFillingToStr(const SampleNeedleFillingStrt &strt)
-{
-    QJsonObject obj;
-    obj.insert("innerTime",strt._innerTime);
-    obj.insert("outerTime",strt._outerTime);
 
     QJsonDocument doc;
     doc.setObject(obj);
@@ -279,10 +260,8 @@ bool ProcessParameterModel::parsingParas()
     }else if(_actCode == TAKE_PHOTO_CODE)
     {
         _paramParseSuccess = true;
-    }else if(_actCode == SAMPLE_NEEDLE_FILLING_CODE)
-    {
-        _paramParseSuccess = strToSampleNeedleFilling(_sampleNeedleFillingStrt, _paras);
     }
+
     return _paramParseSuccess;
 }
 
@@ -387,22 +366,6 @@ void ProcessParameterModel::setDrying(const ProcessParameterModel::DryingStrt &s
     _paras = dryingToStr(strt);
 }
 
-
-bool ProcessParameterModel::getSampleNeedleFilling(SampleNeedleFillingStrt &out)
-{
-    if(!_paramParseSuccess || _actCode!=DRYING_CODE)
-    {
-        return false;
-    }
-    out = _sampleNeedleFillingStrt;
-    return true;
-}
-
-void ProcessParameterModel::setSampleNeedleFilling(const SampleNeedleFillingStrt &strt)
-{
-    _paras = sampleNeedleFillingToStr(strt);
-}
-
 // 界面显示的string
 QString ProcessParameterModel::toShowString()
 {
@@ -460,6 +423,13 @@ QString ProcessParameterModel::toShowString()
         show+="样本量:";
         show+=QString::number(_samplingStrt._sampleUl, 'f', 1);
         show+="ul;";
+        show+="是否样本充盈:";
+        show+="内针冲洗时间:";
+        show+=QString::number(_samplingStrt._innerTime);
+        show+="s;";
+        show+="外针冲洗时间:";
+        show+=QString::number(_samplingStrt._outerTime);
+        show+="s;";
     }else if(_actCode == DRAINING_CODE)
     {
         show+="排废液时间:";
@@ -470,14 +440,6 @@ QString ProcessParameterModel::toShowString()
     {
         show+="暂停流程, 上报信息:";
         show+=_pausingStrt._notifyMessage;
-    }else if(_actCode == SAMPLE_NEEDLE_FILLING_CODE)
-    {
-        show+="内针冲洗时间:";
-        show+=QString::number(_sampleNeedleFillingStrt._innerTime);
-        show+="s;";
-        show+="外针冲洗时间:";
-        show+=QString::number(_sampleNeedleFillingStrt._outerTime);
-        show+="s;";
     }
     else if(_actCode == TAKE_PHOTO_CODE)
     {
